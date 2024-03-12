40b7f5fd2b2fa92ca46edc7c40e065a1c18a3fc8
John Cañete <50263189+superbyte23@users.noreply.github.com>
2024-03-12 12:35:01 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
+function startGame() {
+  const numberToGuess = Math.ceil(Math.random() * 100);
+  let numberGuess = 0;
+  let counter = 0;
 
+  do {
+    if (counter >= 10) {
+      alert("Maximum guess limit reached! Game Over!");
+      break;
+    }
+    counter++;
+    numberGuess = parseInt(
+      prompt(`Guess ${counter} : Enter a number from 1-100`)
+    );
+    if (numberGuess > numberToGuess) {
+      alert("Your number is greater than the guess number. Try again!");
+    } else if (numberGuess < numberToGuess) {
+      alert("Your number is lesser than the guess number. Try again!");
+    } else if (numberGuess == numberToGuess) {
+      alert(
+        `Congratulations! Your guess number ${numberGuess} is correct. Game Over!`
+      );
+      break;
+    } else {
+      alert(
+        "Invalid Input! Please enter numeric value from 1-100 only. Try Again"
+      );
+    }
+  } while (numberGuess != numberToGuess);
+}
